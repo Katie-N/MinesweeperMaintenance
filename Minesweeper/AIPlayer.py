a55ac7cfb9ce49369517285be56367aee509b69e
Katie-N <kaitlyn.nordberg@gmail.com>
2025-09-26 13:47:27 -0500

    I enhanced pattern 1 so it will work whether the covered cells are below the pattern or above it. I tested and it seems to work without crashing

--- Minesweeper/AIPlayer.py
+++ Minesweeper/AIPlayer.py
@@ -49,22 +49,65 @@ class AIPlayer:
         # Logic: Along a wall, if 2 adjacent revealed squares show a 1 and there are 3 covered cells below the squares, then the third square can be safely uncovered. 
         # This logic also goes for when the pattern is rotated in any direction.
 
-        # Loop through each row until it gets to the second to last row. 
-        # This pattern is not possible on the last row because there are no cells under the last row
-       
         # Pattern 1, check by row from left wall. 
         col = 0
-        for row in range(self.board.width - 1):
-            if (currentBoardState[row][col] == 1) and (currentBoardState[row][col+1] == 1) and (currentBoardState[row+1][col+2] == "?"):
-                self.board.reveal_square(col + 2, row + 1)
-                return
+        for row in range(self.board.width):
+            if (currentBoardState[row][col] == 1) and (currentBoardState[row][col+1] == 1):
+                if ((row + 1 < self.board.height) and currentBoardState[row+1][col+2] == "?"):
+                    self.board.reveal_square(col + 2, row + 1)
+                    return
+                elif ((row - 1 >= 0) and currentBoardState[row-1][col+2] == "?"):
+                    self.board.reveal_square(col + 2, row - 1)
+                    return
+           
         # Pattern 1, check by row from right wall. 
         col = self.board.width - 1 # - 1 because the columns are zero indexed. 
-        for row in range(self.board.width - 1):
-            if (currentBoardState[row][col] == 1) and (currentBoardState[row][col-1] == 1) and (currentBoardState[row+1][col-2] == "?"):
-                self.board.reveal_square(col-2, row + 1)
-                return
+        for row in range(self.board.width):
+            if (currentBoardState[row][col] == 1) and (currentBoardState[row][col-1] == 1):
+                if ((row + 1 < self.board.height) and currentBoardState[row+1][col-2] == "?"):
+                    self.board.reveal_square(col-2, row + 1)
+                    return
+                elif ((row - 1 >= 0) and currentBoardState[row-1][col-2] == "?"):
+                    self.board.reveal_square(col-2, row + 1)
+                    return
+                
+        # # Pattern 1, check by column from top wall. 
+        # row = 0 # Lock to the top wall
+        # for col in range(self.board.height - 1):
+        #     if (currentBoardState[row][col] == 1) and (currentBoardState[row+1][col] == 1) and (currentBoardState[row+2][col+1] == "?"):
+        #         self.board.reveal_square(col+1, row+2)
+        #         return
+            
+        # Pattern 1, check by column from bottom wall.
 
+        # Pattern 2, If there is a cell with 3 '1's in around one of its corners, then it must be a mine and shouldd be flagged. 
+        # 1 1
+        # 1 ?
+        # Safe Move:
+        # 1 1
+        # 1 F
+
+        # Pattern 3, similar to pattern 1 but for a mine. If there are 2 consecutive 2's off of a wall with uncovered cells on one side and covered cells on the other side, then the 2 covered cells can be flagged and the third cell can be uncovered.
+        # | * *
+        # | 2 2
+        # | ? ? *
+        # Safe Move:
+        # | * *
+        # | 2 2
+        # | F F X
+
+        # Pattern 4, if the number of flags surrounding an uncovered cell == the number of that cell, then all unflagged covered cells around that cell can be safely uncovered. 
+        # This pattern will fail if the user incorrectly flags a non-mine however this is not a bug but a feature. 
+
+        # Pattern 5, similar to pattern 1 but for mines. If there is a wall with a 1 then a 2 off the wall, the third cell in the adjacent row/column is a mine and should be flagged.
+        # | * * *
+        # | 1 2
+        # | ? ? ?
+        # Safe Move:
+        # | * * *
+        # | 1 2
+        # | ? ? F
+        
     # TODO: Implement actual AI logic for hard mode
     def make_hard_move(self):
         # Implement hard difficulty logic
