074859916f573c2f612cc72e158888f18635a47e
Katie-N <kaitlyn.nordberg@gmail.com>
2025-09-26 13:09:06 -0500

    Added pattern 1 for right wall horizontal moves. Also fixed bug in pattern 1 because the existing logic uses column then row not row then column)

--- Minesweeper/AIPlayer.py
+++ Minesweeper/AIPlayer.py
@@ -14,8 +14,9 @@ class AIPlayer:
         elif self.difficulty == "Hard":
             self.make_hard_move()
     
-    # To uncover a cell just do self.board.reveal_square(x, y) 
-    # To flag a cell just do self.toggle_flag(x,y)
+    # NOTE: Their logic takes the column first then the row. No idea why since it is usually row then column. Don't get tripped up by this. 
+    # To uncover a cell just do self.board.reveal_square(column, row) 
+    # To flag a cell just do self.toggle_flag(column, row)
 
     # TODO: Implement actual AI logic for easy mode
     def make_easy_move(self):
@@ -48,14 +49,20 @@ class AIPlayer:
         # Logic: Along a wall, if 2 adjacent revealed squares show a 1 and there are 3 covered cells below the squares, then the third square can be safely uncovered. 
         # This logic also goes for when the pattern is rotated in any direction.
 
-        # Start with the first row then abstract it
-        col = 0
         # Loop through each row until it gets to the second to last row. 
         # This pattern is not possible on the last row because there are no cells under the last row
+       
+        # Pattern 1, check by row from left wall. 
+        col = 0
         for row in range(self.board.width - 1):
             if (currentBoardState[row][col] == 1) and (currentBoardState[row][col+1] == 1) and (currentBoardState[row+1][col+2] == "?"):
-                print(f"Uncovering {row + 1} and {col + 2}")
-                self.board.reveal_square(row + 1, col + 2)
+                self.board.reveal_square(col + 2, row + 1)
+                return
+        # Pattern 1, check by row from right wall. 
+        col = self.board.width - 1 # - 1 because the columns are zero indexed. 
+        for row in range(self.board.width - 1):
+            if (currentBoardState[row][col] == 1) and (currentBoardState[row][col-1] == 1) and (currentBoardState[row+1][col-2] == "?"):
+                self.board.reveal_square(col-2, row + 1)
                 return
 
     # TODO: Implement actual AI logic for hard mode
